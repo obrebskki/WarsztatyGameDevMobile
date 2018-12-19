da0ada38ce53bedb07299743f03d9459e19cdf8a
obamski <obrebskirafal@gmail.com>
2018-12-19 19:09:45 +0100

    Added simple main menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public void LoadGameplayScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
